c8562f6dd71b931ff6283da78d4fc735559a48de
Connor <crichmondclark@protonmail.com>
2020-02-24 20:05:51 +0000

    fix

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import Navigation from '../components/navigation';
-import Footer from '../components/footer';
-import ProjectCard from '../components/projectCard';
-import '../sass/projects.scss';
-import calcImg from '../../static/calcthumb.png';
-import conqImg from '../../static/conq.png';
+import React from "react"
+import Navigation from "../components/navigation"
+import Footer from "../components/footer"
+import ProjectCard from "../components/projectCard"
+import "../sass/projects.scss"
+import calcImg from "../../static/calcthumb.png"
+import conqImg from "../../static/conq.png"
 
 const ProjectsPage = () => {
   const projects = [
     {
-      title: 'Calculator',
+      title: "Calculator",
       img: calcImg,
-      alt: 'a calculator',
+      alt: "a calculator",
       desc:
-        'I decided to use this project to practice what I had learned about React and experiement with CSS in JS using the styled components library. In addition to learning react, it also helped me realise the benefits of using multiple git branches during development.',
-      live: 'https://musing-boyd-4d0b55.netlify.com/',
-      github: 'https://github.com/crichmond-clark/react-calculator'
+        "I decided to use this project to practice what I had learned about React and experiement with CSS in JS using the styled components library. In addition to learning react, it also helped me realise the benefits of using multiple git branches during development.",
+      live: "https://musing-boyd-4d0b55.netlify.com/",
+      github: "https://github.com/crichmond-clark/react-calculator",
     },
     {
-      title: 'Conquerer Template Clone',
+      title: "Conquerer Template Clone",
       img: conqImg,
-      alt: 'conquer template clone',
+      alt: "conquer template clone",
       desc:
-        'I created this project using HTML and Sass using CSS Grid for the responsive layout element rather than the original template choice of bootstrap. I have personally found CSS Grid to be the most intuitive and simple way to create responsive layouts in addition to using flexbox for some more niche use cases such as navigation bars.',
-      live: 'https://crichmond-clark.github.io/conquer-template-clone',
-      github: 'https://github.com/crichmond-clark/conquer-template-clone'
-    }
-  ];
+        "I created this project using HTML and Sass using CSS Grid for the responsive layout element rather than the original template choice of bootstrap. I have personally found CSS Grid to be the most intuitive and simple way to create responsive layouts in addition to using flexbox for some more niche use cases such as navigation bars.",
+      live: "https://crichmond-clark.github.io/conquer-template-clone",
+      github: "https://github.com/crichmond-clark/conquer-template-clone",
+    },
+  ]
   return (
     <div className="container">
       <Navigation />
@@ -45,7 +45,7 @@ const ProjectsPage = () => {
       </main>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default ProjectsPage;
+export default ProjectsPage
