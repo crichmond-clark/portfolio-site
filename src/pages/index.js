fb6e48dac56b3060ab1c38afa03af7af69deff8f
crichmond-clark <connor@Connors-MacBook-Pro.local>
2020-09-08 17:06:10 +0100

    osano test 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import CC from "CookieConsent"
-import {Helmet} from "react-helmet";
+// import {Helmet} from "react-helmet";
+import { useEffect } from 'react';
 import Navigation from '../components/navigation';
 import Footer from '../components/footer';
 import '../sass/global.scss';
@@ -8,7 +9,21 @@ import '../sass/index.scss';
 
 const IndexPage = () => {
 
-
+  const useScript = url => {
+    useEffect(() => {
+      const script = document.createElement('script');
+  
+      script.src = url;
+      script.async = true;
+  
+      document.head.prepend(script);
+  
+      return () => {
+        document.head.remove(script);
+      }
+    }, [url]);
+  };
+  useScript("https://cmp.osano.com/Azyw5ISA0ipuZfJQ/a4a0e116-0a11-4f54-a918-48e7ff1f2ae8/osano.js");
 
   return (
     <div className="container">
