6578c1c2c799ef47e70dc3f8703ff1649ed3334f
crichmond-clark <connor@Connors-MacBook-Pro.local>
2020-09-08 16:54:02 +0100

    cookie consent test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+// import CC from "CookieConsent"
+import {Helmet} from "react-helmet";
 import Navigation from '../components/navigation';
 import Footer from '../components/footer';
 import '../sass/global.scss';
 import '../sass/index.scss';
 
 const IndexPage = () => {
+
+
+
   return (
     <div className="container">
+      <Helmet>
+      <script src="https://cmp.osano.com/Azyw5ISA0ipuZfJQ/a4a0e116-0a11-4f54-a918-48e7ff1f2ae8/osano.js"></script>
+      </Helmet>
       <Navigation />
       <main className="main">
         <div>
